a98982f8827c03ec2642cc01083e219d207f371c
manihamidi <mani.hamidi@abcellera.com>
2017-12-01 17:28:19 -0800

    add function call from __init__ to test upon import of privateliba

---
+++ privateliba/__init__.py
@@ -0,0 +1,5 @@
+from privateliba.main import hello_from_a
+
+hello_from_a()
+
+
--- privateliba/main.py
+++ privateliba/main.py
@@ -2,8 +2,11 @@ def hello_from_a():
     print("We are in library A. \n"
           "This library is dependent on another private library: 'privatelibb'")
 
-    from privatelibb.main import hello_from_b
-    hello_from_b()
+    try:
+        from privatelibb.main import hello_from_b
+        hello_from_b()
+    except Exception as e:
+        print("Oops! Doesn't look like the dependency got installed!")
 
 hello_from_a()
 
